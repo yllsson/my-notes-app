de23e507d6292998c3600bb2c7809c9fc3c46050
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-06-21 14:42:55 +0100

    expanding the addNote function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* The app.js file will contain all my yargs commands and take in the functions from notes.js to manipulate the data from the command line */
 const yargs = require('yargs');
 const chalk = require('chalk');
+const notes = require('./notes');
 
 // I want 4 commands - add, remove, read, list
 yargs.command({
@@ -11,10 +12,16 @@ yargs.command({
       describe: 'Title of the note',
       demandOption: true,
       type: 'string'
+    },
+    body: {
+      describe: 'Body of the note',
+      demandOption: true,
+      type: 'string'
     }
   },
   handler(argv) {
-    console.log(chalk.magenta.inverse('Adding a note'), argv.title);
+    notes.addNote(argv.title, argv.body);
+    console.log(chalk.magenta.inverse('Adding a note'));
   }
 });
 
--- notes.js
+++ notes.js
@@ -1,1 +1,17 @@
 /* The notes.js file will list all my functions for the command handlers */
+const fs = require('fs');
+
+// addNote
+const addNote = (title, body) => {
+  const note = {
+    title,
+    body
+  };
+
+  const stringJSON = JSON.stringify(note);
+  fs.writeFileSync('notes.json', stringJSON);
+};
+
+module.exports = {
+  addNote
+};
